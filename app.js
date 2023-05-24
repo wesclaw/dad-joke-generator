1ca0f560f8359731495c802f4f665fbfe78b27e6
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-05-24 21:34:09 +0200

    practiced again

--- app.js
+++ app.js
@@ -3,22 +3,20 @@ const joke_text = document.getElementById('joke-text')
 
 const apiKey = '/WprvfrOrzw8t7ltSSgzwg==l1jkAY7GxLu37K3m'
 
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
 const options = {
   method: 'GET',
-  headers: {
-    'X-Api-Key': apiKey,
+  headers:{
+    'X-API-Key': apiKey,
   },
 }
 
-const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
-
 async function getJoke(){
-
   joke_text.textContent = 'Thinking...'
-  const repsonse = await fetch(apiURL, options)
-  const data = await repsonse.json()
+  const response = await fetch(apiURL, options)
+  const data = await response.json()
   joke_text.textContent = data[0].joke
 }
 
 joke_btn.addEventListener('click', getJoke)
-
