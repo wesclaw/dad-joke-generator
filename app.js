d860a35d750fbb36619a0214dc01fb9e0764ed6f
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-05-26 18:31:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-const joke_btn = document.getElementById('joke_btn')
+// const joke_btn = document.getElementById('joke_btn')
+// const joke_text = document.getElementById('joke-text')
+
+// const apiKey = '/WprvfrOrzw8t7ltSSgzwg==l1jkAY7GxLu37K3m'
+
+// const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+// const options = {
+//   method: 'GET',
+//   headers:{
+//     'X-API-Key': apiKey,
+//   },
+// }
+
+// async function getJoke(){
+//   joke_text.textContent = 'Thinking...'
+//   const response = await fetch(apiURL, options)
+//   const data = await response.json()
+//   joke_text.textContent = data[0].joke
+// }
+
+// joke_btn.addEventListener('click', getJoke)
+
+
+
+
 const joke_text = document.getElementById('joke-text')
+const joke_btn = document.getElementById('joke_btn')
+
 
 const apiKey = '/WprvfrOrzw8t7ltSSgzwg==l1jkAY7GxLu37K3m'
 
 const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
 
+
 const options = {
   method: 'GET',
-  headers:{
+  headers: {
     'X-API-Key': apiKey,
-  },
+  }
 }
 
 async function getJoke(){
   joke_text.textContent = 'Thinking...'
-  const response = await fetch(apiURL, options)
-  const data = await response.json()
+  const res = await fetch(apiURL, options)
+  const data = await res.json()
   joke_text.textContent = data[0].joke
 }
 
